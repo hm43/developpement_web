99ba1202d8b4697cc9df900c821a4b1b057c1419
Houda Mouttalib <houda.mouttalib@gmail.com>
2025-11-10 10:42:44 +0100

    course updated and exercice

---
+++ Chapitre1/Cours/seance6_js/script.js
@@ -0,0 +1,8 @@
+let x = 4;
+
+let test1 = x == '4';
+let test2 = x === '4';
+// Le reste de l'exercice..
+console.log('valeur de x == "4" est ' + test1 +
+    '\nvaleur de x === "4" est ' + test2
+)
